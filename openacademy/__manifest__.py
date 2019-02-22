bec116ffa8449ac3d478b58068c0c7bb557a2693
testodoo <47593912+testodoo@users.noreply.github.com>
2019-02-22 09:29:13 +0100

    correction of depends

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'portal', 'web', 'web_kanban'],
+    'depends': ['base', 'mail', 'portal', 'web', 'web_kanban_gauge'],
 
     # always loaded
     'data': [
